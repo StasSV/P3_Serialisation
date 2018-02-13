6af5e0d6d6f64cadfc1907aebd0e9949eef28225
Stepaniuk Stanislav <stanislav.stepaniuk@softline.kiev.ua>
2018-02-13 22:57:49 +0200

    Serialisation_done

--- Program.cs
+++ Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
 
 namespace P3_Serialisation
 {
 
-    
-    class Student
+   [Serializable]
+    public class Student
     {
-        public string Family  {get; private set;}
-        public IDictionary<string, int> Subj { get; private set; }
-        public double AVG {get; private set;}
-        
-        public Student(string Family, IDictionary<string, int> Subj)
-        {
-            this.Family = Family;
-            this.Subj = Subj;
-            double AVG = 0;
-            foreach (var s in Subj)
-                AVG += s.Value;
-            this.AVG = AVG / this.Subj.Count;
+        public string name  {get; set;}
+        public string surName  {get; set;}
+        public string faculty  {get; set;}
+        public string direction {get; set;}
+        public int course  {get; set;}
+        public int group  {get; set;}
+        public int studcard   {get; set;}
 
-        }
-        public override string ToString()
+        public Student()
         {
-            string res = Family + "  ";
-            foreach (var s in Subj)
-                res += " "+ s.Value+ "   ";
-            res +=  AVG;
-            return res;
         }
-        
-        public static bool StudentAVG(Student obj1, Student obj2)
+
+        public Student(string name,string surName,string faculty,string direction,int course,int group,int studcard)
         {
-            return obj1.AVG < obj2.AVG;
+            this.name = name;
+            this.surName = surName;
+            this.faculty = faculty;
+            this.direction = direction;
+            this.course = course;
+            this.group = group;
+            this.studcard = studcard;
+            
+
         }
     }
     
@@ -42,36 +41,68 @@ namespace P3_Serialisation
     {
         static void Main()
         {
-            IDictionary<string, int>[] st = { new Dictionary<string, int>(),
-                new Dictionary<string, int>(),
-                new Dictionary<string, int>(),
-                new Dictionary<string, int>()};
+            Collection <Student> students = new Collection <Student>();
+            students.Add(new Student("John","Kennedy","Informatics","Applied Mathematics",1,1,100000111));
+            students.Add(new Student("Rick","Nilson","Informatics","Applied Mathematics",2,1,100000211));
+            students.Add(new Student("Morty","Fox","Informatics","Applied Mathematics",1,2,100000121));
+            students.Add(new Student("Nill","Armstrong","Informatics","Applied Mathematics",3,2,100000321));
+            students.Add(new Student("Barry","Hills","Informatics","Applied Mathematics",4,1,100000411));
+            students.Add(new Student("Don","Shon","Informatics","Applied Mathematics",2,1,100000212));
+            students.Add(new Student("Peter","Pin","Informatics","Applied Mathematics",1,4,100000141));
+           
             
-            Random rnd = new Random();
-            
-            foreach (var s in st)
+            foreach (Student s in students)
             {
-                s.Add("Math", 100 + rnd.Next(0,50)*(-1)^rnd.Next(2,3)-2);
-                s.Add("Java", 100 + rnd.Next(0,50)*(-1)^rnd.Next(2,3)-2);
-                s.Add("Ukr", 100 + rnd.Next(0,50)*(-1)^rnd.Next(2,3)-2);
-                s.Add("TFKZ", 100 + rnd.Next(0,50)*(-1)^rnd.Next(2,3)-2);
+                Console.WriteLine("Name : {0}   Surname : {1}   Faculty of : {2}   Direction : {3}   Course :{4}   Group :{5}    StudentCard :{6}   ", s.name,s.surName,s.faculty,s.direction,s.course,s.group,s.studcard);
             }
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++Serialisation process is runnning ..... ++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+            BinaryFormatter bf1 = new BinaryFormatter();
+            using (FileStream f1 = new FileStream("Students.dat", FileMode.OpenOrCreate))
+            {
+                bf1.Serialize(f1, students);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Binary Serialisation ....................DONE ");
+            Console.WriteLine();
+            XmlSerializer xs1 = new XmlSerializer(typeof(Collection<Student>));
+            using (FileStream f2 = new FileStream("Students.xml", FileMode.OpenOrCreate))
+            {
+                xs1.Serialize(f2, students);
+            }
+            Console.WriteLine();
+            Console.WriteLine("XML Serialisation .......................DONE ");
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++Deserialisation process is runnning ..... ++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+            using (FileStream f3 = new FileStream("Students.dat", FileMode.OpenOrCreate))
+            {
+                Collection<Student> studentsnew = (Collection<Student>)bf1.Deserialize(f3);
+                foreach (Student s in studentsnew)
+                {
+                    Console.WriteLine("Name : {0}   Surname : {1}   Faculty of : {2}   Direction : {3}   Course :{4}   Group :{5}    StudentCard :{6}   ", s.name,s.surName,s.faculty,s.direction,s.course,s.group,s.studcard);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Binary Deserialisation ....................DONE ");
+            Console.WriteLine();
             
-            Student[] studentinfo = { new Student("Spring",st[0]),
-                                  new Student("Winter",st[1]),
-                                  new Student("Ivanov",st[2]),
-                                  new Student("Coyote",st[3])};
-            
-        
+            using (FileStream f4 = new FileStream("Students.xml", FileMode.OpenOrCreate))
+            {
+                Collection<Student> studentsnew = (Collection<Student>)xs1.Deserialize(f4);
 
-            Console.WriteLine("Sorting students by average grades: \n" +
-                "-------------------------------------\n"+
-                "Family  Math  Java  Ukr  TFKZ  Average\n  ");
-            
-            foreach (var ui in studentinfo)
-                Console.WriteLine(ui);
-
+                foreach (Student s in studentsnew)
+                {
+                    Console.WriteLine("Name : {0}   Surname : {1}   Faculty of : {2}   Direction : {3}   Course :{4}   Group :{5}    StudentCard :{6}   ", s.name,s.surName,s.faculty,s.direction,s.course,s.group,s.studcard);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("XML Deserialisation ....................DONE ");
+            Console.WriteLine();
             Console.ReadLine();
+            
+            
         }
     }
 }
